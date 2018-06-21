036273fe9ba00c6609bbe3f05ad51e57ec7f39c3
Nazar-bnm <nazar.bnm@gmail.com>
2018-06-21 11:48:03 +0300

    add scss preprocessor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './App.css';
 
 const App = () => (
   <div className="App">
-    <h1>Allocation planing tool</h1>
+    <h1 className="welcome">Allocation planing tool</h1>
   </div>
 );
 
