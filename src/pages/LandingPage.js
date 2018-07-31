3f39590109eff0d046543c78e8cf91c49c5795b6
Nazar-bnm <nazar.bnm@gmail.com>
2018-07-31 12:18:26 +0300

    add employees page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LandingPage = () => (
   <div>
-    <h1>Landing Page</h1>
+    <h1>Welcome to Ralabs Allocations Planning Tools</h1>
   </div>
 );
 
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import DashboardPage from './DashboardPage';
+import EmployeesPage from './EmployeesPage';
+import LandingPage from './LandingPage';
+
+export {
+  DashboardPage,
+  EmployeesPage,
+  LandingPage,
+};
