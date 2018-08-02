fa760c22bc9e7cd31a16112def7bf6a7c972939b
Nazar-bnm <nazar.bnm@gmail.com>
2018-08-02 13:35:47 +0300

    add filtering and sorting on employee page

--- src/pages/EmployeesPage.js
+++ src/pages/EmployeesPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 // components
 import EmployeesList from '../components/employees/EmployeesList';
@@ -11,6 +12,8 @@ class EmployeesPage extends Component {
   static propTypes = {
     getUsers: PropTypes.func.isRequired,
     users: PropTypes.arrayOf(PropTypes.object).isRequired,
+    searchData: PropTypes.objectOf(PropTypes.array).isRequired,
+    sortUp: PropTypes.bool.isRequired,
   };
 
   componentDidMount() {
@@ -18,9 +21,14 @@ class EmployeesPage extends Component {
   }
 
   render() {
+    const { users: employees, sortUp, searchData } = this.props;
+    const sortedEmployees = sortUp ? employees : _.clone(employees).reverse();
+    const filteredEmployees = !searchData.employeesIds ?
+      sortedEmployees : sortedEmployees.filter(item => searchData.employeesIds.includes(item._id));
+
     return (
       <div>
-        { this.props.users.length && <EmployeesList users={this.props.users} /> }
+        {this.props.users.length && <EmployeesList users={filteredEmployees} /> }
       </div>
     );
   }
@@ -28,6 +36,8 @@ class EmployeesPage extends Component {
 
 const mapStateToProps = state => ({
   users: state.users.allUsers,
+  searchData: state.search.searchData,
+  sortUp: state.search.sortUp,
 });
 
 const mapDispatchToProps = dispatch => ({
